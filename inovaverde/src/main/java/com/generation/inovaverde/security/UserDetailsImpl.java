bdc628107f8a9754b0387f065970030b18a64e88
Asttryd S. Pacheco <95387263+Asttryd@users.noreply.github.com>
2024-03-27 17:25:42 -0300

    Update UserDetailsImpl.java

--- inovaverde/src/main/java/com/generation/inovaverde/security/UserDetailsImpl.java
+++ inovaverde/src/main/java/com/generation/inovaverde/security/UserDetailsImpl.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.generation.blogpessoal.model.Usuario;
+import com.generation.inovaverde.model.Usuario;
 
 public class UserDetailsImpl implements UserDetails{
 
