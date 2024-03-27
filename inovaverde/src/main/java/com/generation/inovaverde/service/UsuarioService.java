c6217120f39a814d5fb32c8bbbbade97b31adae1
Asttryd S. Pacheco <95387263+Asttryd@users.noreply.github.com>
2024-03-27 17:21:49 -0300

    Update UsuarioService.java

--- inovaverde/src/main/java/com/generation/inovaverde/service/UsuarioService.java
+++ inovaverde/src/main/java/com/generation/inovaverde/service/UsuarioService.java
@@ -11,8 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.generation.inovaverde.model.Usuario;
 import com.generation.inovaverde.model.UsuarioLogin;
+import com.generation.inovaverde.model.Usuario;
 import com.generation.inovaverde.repository.UsuarioRepository;
 import com.generation.inovaverde.security.JwtService;
 
@@ -60,22 +60,29 @@ public class UsuarioService {
 
 	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
         
+        // Gera o Objeto de autenticação
 		var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
 		
+        // Autentica o Usuario
 		Authentication authentication = authenticationManager.authenticate(credenciais);
         
+        // Se a autenticação foi efetuada com sucesso
 		if (authentication.isAuthenticated()) {
 
+            // Busca os dados do usuário
 			Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
 
+            // Se o usuário foi encontrado
 			if (usuario.isPresent()) {
 
-			    usuarioLogin.get().setId(usuario.get().getId());
+                // Preenche o Objeto usuarioLogin com os dados encontrados 
+			   usuarioLogin.get().setId(usuario.get().getId());
                 usuarioLogin.get().setNome(usuario.get().getNome());
                 usuarioLogin.get().setFoto(usuario.get().getFoto());
                 usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getUsuario()));
                 usuarioLogin.get().setSenha("");
 				
+                 // Retorna o Objeto preenchido
 			   return usuarioLogin;
 			
 			}
