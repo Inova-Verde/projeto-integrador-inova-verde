2fd0fba9af9205d0d9a6fb82f87135d737c95c2c
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-03-27 14:41:37 -0300

    remoção na annotation repository das interfaces

--- inovaverde/src/main/java/com/generation/inovaverde/repository/UsuarioRepository.java
+++ inovaverde/src/main/java/com/generation/inovaverde/repository/UsuarioRepository.java
@@ -4,12 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.generation.inovaverde.model.Usuario;
 
-
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	public Optional<Usuario> findByUsuario(@Param("usuario") String usuario);
--- inovaverde/src/main/java/com/generation/inovaverde/security/UserDetailsImpl.java
+++ inovaverde/src/main/java/com/generation/inovaverde/security/UserDetailsImpl.java
@@ -14,56 +14,47 @@ public class UserDetailsImpl implements UserDetails{
 	
 	private String userName;
 	private String password;
-	
 	private List<GrantedAuthority> authorities;
 	
 	public UserDetailsImpl(Usuario user) {
 		this.userName = user.getUsuario();
-		this.password = user.getSenha();
-		
+		this.password = user.getSenha();	
 	}
 	
 	public UserDetailsImpl() {}
 	
 	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
+	public Collection<? extends GrantedAuthority> getAuthorities() {	
 		return authorities;
 	}
 
 	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
+	public String getPassword() {	
 		return password;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return userName;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
+	public boolean isAccountNonLocked() {	
 		return true;
 	}
 
 	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
+	public boolean isCredentialsNonExpired() {	
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
